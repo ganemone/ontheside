57e40f1bd3b45749446f12608478866e789e74a3
Giancarlo Anemone <ganemone@gmail.com>
2015-01-28 15:07:45 -0500

    Fix validators on RegistrationForm

--- server/forms/RegistrationForm.py
+++ server/forms/RegistrationForm.py
@@ -3,15 +3,19 @@ from wtforms import Form, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
     username = StringField(
-        [validators.Required(), validators.Length(min=4, max=25)]
+        'username',
+        [validators.required(),
+         validators.Length(min=4, max=25)]
     )
     password = PasswordField(
-        [validators.Required(),
+        'password',
+        [validators.required(),
          validators.Length(min=6, max=35),
          validators.EqualTo('confirm')]
     )
-    confirm = PasswordField()
-    email = StringField([
-        validators.Required(),
-        validators.length(min=6, max=35)]
+    confirm = PasswordField('confirm')
+    email = StringField(
+        'email',
+        [validators.required(),
+         validators.length(min=6, max=35)]
     )
