6a419c4a09e282f7c0ddc137ad8090a40007b592
Giancarlo Anemone <ganemone@gmail.com>
2015-01-28 14:21:55 -0500

    Moved auth to mod_auth folder

---
+++ server/mod_auth/auth.py
@@ -0,0 +1,27 @@
+from app_factory import db
+from db.models.User import User
+from forms.RegistrationForm import RegistrationForm
+from forms.LoginForm import LoginForm
+
+
+def load_user(user_id):
+    return User.query.filter_by(id=user_id)
+
+
+def register(request):
+    form = RegistrationForm.from_json(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(
+            name=form.name,
+            username=form.username,
+            password=form.password,
+            email=form.email
+        )
+        db.session.add(user)
+        db.session.commit()
+
+
+def login(request):
+    form = LoginForm.from_json(request.form)
+    if request.method == 'POST' and form.validate():
+        # Do login stuff here
