5b00010451f9ea58936f98b72737a646d77e1bd9
Giancarlo Anemone <ganemone@gmail.com>
2015-01-28 15:06:47 -0500

    Add tests for failing registration forms

--- server/tests/forms/test_RegistrationForm.py
+++ server/tests/forms/test_RegistrationForm.py
@@ -1,9 +1,6 @@
-import wtforms_json
 import pytest
 
 from forms.RegistrationForm import RegistrationForm
-
-wtforms_json.init()
 
 
 class TestRegistrationForm:
@@ -12,9 +9,55 @@ class TestRegistrationForm:
         json = {
             'username': 'someusername',
             'password': 'password',
-            'confirm': 'confirm',
+            'confirm': 'password',
             'email': 'someemail@email.com'
         }
 
         form = RegistrationForm.from_json(json)
         assert (form.validate())
+
+    def test_missing_username(self):
+        json = {
+            'password': 'password',
+            'confirm': 'password',
+            'email': 'someemail@email.com'
+        }
+        form = RegistrationForm.from_json(json)
+        assert form.validate() is False
+
+    def test_missing_password(self):
+        json = {
+            'username': 'someusername',
+            'confirm': 'password',
+            'email': 'someemail@email.com'
+        }
+        form = RegistrationForm.from_json(json)
+        assert form.validate() is False
+
+    def test_missing_confirm(self):
+        json = {
+            'username': 'someusername',
+            'password': 'password',
+            'email': 'someemail@email.com'
+        }
+        form = RegistrationForm.from_json(json)
+        assert form.validate() is False
+
+    def test_missing_email(self):
+        json = {
+            'username': 'someusername',
+            'password': 'password',
+            'confirm': 'password',
+        }
+        form = RegistrationForm.from_json(json)
+        assert form.validate() is False
+
+    def test_invalid_password_combination(self):
+        json = {
+            'username': 'someusername',
+            'password': 'password',
+            'confirm': 'WRONG',
+            'email': 'someemail@email.com'
+        }
+        form = RegistrationForm.from_json(json)
+        assert form.validate() is False
