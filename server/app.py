4c908b20b3704e436813df737b7adfd8b6fe16e0
Giancarlo Anemone <ganemone@gmail.com>
2015-01-30 22:29:12 -0500

    Refactored api configuration out of app.py

--- server/app.py
+++ server/app.py
@@ -1,21 +1,14 @@
 from flask import render_template
 from app_factory import create_app, create_api, db
-from models import User, Keyword, Language, Project
+from api import api_config
 
 # Create the flask app
 app = create_app()
 
-api_manager = create_api(app, db, [
-    {'model': User, 'methods': ['GET', 'POST', 'DELETE']},
-    {'model': Keyword, 'methods': ['GET', 'POST', 'DELETE']},
-    {'model': Language, 'methods': ['GET', 'POST', 'DELETE']},
-    {'model': Project, 'methods': ['GET', 'POST', 'DELETE'],
-        'include_columns': ['id', 'description',
-                            'source', 'type']}
-])
+api_manager = create_api(app, db, api_config)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -32,8 +25,6 @@ def static_proxy(file_path, extension):
     file_path = file_path.rstrip('/') + "." + extension
     app.logger.info('File Path: %s' % file_path)
     return app.send_static_file(file_path)
-
-some_user = User.query.filter_by(id=1)
 
 if __name__ == "__main__":
     app.run(debug=True)
