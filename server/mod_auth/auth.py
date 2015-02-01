5ac6cc208bf1a3fbe4e860a2356102a2457a1e43
Giancarlo Anemone <ganemone@gmail.com>
2015-02-01 17:15:32 -0500

    Clean up unused imports and add docstrings

--- server/mod_auth/auth.py
+++ server/mod_auth/auth.py
@@ -1,13 +1,14 @@
-from app_factory.create_app import db
 from models import User
-from forms import RegistrationForm, LoginForm
+from forms import LoginForm
 
 
 def load_user(user_id):
+    """Returns a user from the database based on their id"""
     return User.query.filter_by(id=user_id).first()
 
 
 def login(request):
+    """Handle a login request from a user."""
     form = LoginForm.from_json(request.form)
     if request.method == 'POST' and form.validate():
         return True
