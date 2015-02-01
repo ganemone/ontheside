23604efc203f62f1059c4bd18f233cccdaf045e6
Giancarlo Anemone <ganemone@gmail.com>
2015-02-01 17:12:45 -0500

    Add login manager initialization to app creation method

--- server/app_factory/create_app.py
+++ server/app_factory/create_app.py
@@ -1,12 +1,19 @@
 import wtforms_json
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
+# Create db object so it can be shared throughout the application
 db = SQLAlchemy()
+
+# Create the login manager to be shared throughout the application
+login_manager = LoginManager()
 
 
 def create_app():
-
+    """Creates and returns the Flask WSGI application
+    and initializes helping components"""
+    # Initialize json support for wtforms
     wtforms_json.init()
 
     # Define the WSGI Application object
@@ -19,8 +26,12 @@ def create_app():
     # Configurations
     app.config.from_object('config')
 
+    # Initialize database with application
     db.init_app(app)
     with app.test_request_context():
         db.create_all()
+
+    # Initialize login manager with application
+    login_manager.init_app(app)
 
     return app
