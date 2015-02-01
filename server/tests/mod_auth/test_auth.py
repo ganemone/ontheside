5ba2c2ed969147a3bdbfd765346fbaa52efc5930
Giancarlo Anemone <ganemone@gmail.com>
2015-02-01 16:14:19 -0500

    Add test for loading a user not existing in db

--- server/tests/mod_auth/test_auth.py
+++ server/tests/mod_auth/test_auth.py
@@ -6,11 +6,19 @@ from tests.helpers import FlaskTestCase, fixtures
 class TestAuth(FlaskTestCase):
 
     @fixtures('single_user.json')
-    def test_load_user(self):
+    def test_load_existing_user(self):
+        """Test loading a single valid user"""
         with self.flaskapp.test_request_context():
             user = load_user(1)
             assert user is not None
             assert user.username == 'ganemone'
+
+    @fixtures('base.json')
+    def test_load_nonexisting_user(self):
+        """Test loading a user not in the database"""
+        with self.flaskapp.test_request_context():
+            user = load_user(50)
+            assert user is None
 
 
 class TestPostUser(FlaskTestCase):
