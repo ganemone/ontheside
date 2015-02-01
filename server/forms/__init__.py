148c5b38e4edb1112a37acf8b056275e77af09ec
Giancarlo Anemone <ganemone@gmail.com>
2015-02-01 18:32:01 -0500

    Finish db_match custom validator

--- server/forms/__init__.py
+++ server/forms/__init__.py
@@ -1,2 +1,3 @@
+from forms.db_match import db_match
 from forms.login import LoginForm
 from forms.registration import RegistrationForm
--- server/forms/db_match.py
+++ server/forms/db_match.py
@@ -1,13 +1,18 @@
 from wtforms.validators import ValidationError
 
 
-def db_match(model, message, *args, **kwargs):
-    fields = kwargs.pop('match_fields', None)
-    result = model.query.filter_by(*args, **kwargs).first()
-    if result is None:
-        raise ValidationError(message)
-    if fields is not None:
-        for key in fields:
-            if (not hasattr(result, key) or
-                    result.key != fields[key]):
-                raise ValidationError(message)
+def db_match(model, message, query_fields, match_fields=None):
+    """Custom validator for matching fields in a database"""
+    def _db_match(form, field):
+        """Validator function called by the wtforms api"""
+        query_args = {field: form[field].data for field in query_fields}
+        result = model.query.filter_by(**query_args).first()
+        if result is None:
+            raise ValidationError(message)
+        if match_fields is not None:
+            for key in match_fields:
+                if (not hasattr(result, key) or
+                        result.key != match_fields[key]):
+                    raise ValidationError(message)
+
+    return _db_match
