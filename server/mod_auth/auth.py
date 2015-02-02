e4be6cbd0e814128f173482069b9103815ee6dee
Giancarlo Anemone <ganemone@gmail.com>
2015-02-02 13:19:30 -0500

    Implemented login route handling

--- server/mod_auth/auth.py
+++ server/mod_auth/auth.py
@@ -1,7 +1,10 @@
+from flask import Response
+from flask.ext.login import login_user
+from server.app_factory import login_manager
 from server.models import User
-from server.forms import LoginForm
 
 
+@login_manager.user_loader
 def load_user(user_id):
     """Returns a user from the database based on their id"""
     return User.query.filter_by(id=user_id).first()
@@ -9,6 +12,14 @@ def load_user(user_id):
 
 def login(request):
     """Handle a login request from a user."""
-    form = LoginForm.from_json(request.form)
-    if request.method == 'POST' and form.validate():
-        return True
+    user = User.query.filter_by(
+        username=request.authorization.username,
+        password=request.authorization.password
+    ).first()
+    if user is not None:
+        login_user(user)
+        return 'OK'
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
