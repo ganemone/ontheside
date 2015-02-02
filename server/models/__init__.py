da36c0aefab60f5834a90bc0048d57d76f777076
Giancarlo Anemone <ganemone@gmail.com>
2015-02-02 13:58:07 -0500

    Moving db initialization into the model __init__.py file

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -3,3 +3,6 @@ from server.models.keyword import Keyword
 from server.models.language import Language
 from server.models.project import Project
 from server.models.session import Session
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLALchemy()
