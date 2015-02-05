2e43bcd61965bebe91817f80d52f8f8849a23abe
Giancarlo Anemone <ganemone@gmail.com>
2015-02-05 13:21:26 -0500

    Reformatting routes file

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, request
+
 from server.mod_auth.auth import login
 
 
 def define_routes(app):
-
     @app.route('/')
     def index():
         return render_template('index.html')
