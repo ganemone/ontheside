35c128d3257a6a587030b1d74bd32514bdc6447c
Giancarlo Anemone <ganemone@gmail.com>
2015-02-06 11:41:53 -0500

    Remove session from db models

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -2,7 +2,6 @@ from server.models.db import db
 from server.models.keyword import Keyword
 from server.models.language import Language
 from server.models.project import Project
-from server.models.session import Session
 from server.models.user import User
 
 
