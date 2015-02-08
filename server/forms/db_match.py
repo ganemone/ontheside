6987e2739dd7efef2e4e6b89dd43610b6444acf1
Giancarlo Anemone <ganemone@gmail.com>
2015-02-08 14:58:39 -0500

    Using type declarations rather than specifying types in docs

--- server/forms/db_match.py
+++ server/forms/db_match.py
@@ -1,27 +1,24 @@
+from wtforms import Form, Field
+from flask_sqlalchemy import Model
+from types import FunctionType
 from wtforms.validators import ValidationError
 
 
-def db_match(model, message, query_fields, match_fields=None):
+def db_match(model: Model, message: str, query_fields: list, match_fields: list=None) -> FunctionType:
     """Custom validator for matching fields in a database
 
     :param model: model to execute the query on
-    :type model: flask.ext.sqlalchemy.Model
     :param message:
-    :type message: str
     :param query_fields: list of fields to query against
-    :type query_fields: list
     :param match_fields: list of fields to match against
-    :type match_fields: list
     :return: function
     """
 
-    def _db_match(form, field):
+    def _db_match(form: Form, field: Field) -> None:
         """Validator function called by the wtforms api
 
         :param form: wtform on which validation is called
-        :type form: wtforms.form
         :param field: form field being validated
-        :type field: wtforms.Field
         :raises ValidationError: if the form field fails the validation
         :return: None
         """
