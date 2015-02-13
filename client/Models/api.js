530053058cfb80248070cf6e943a575af11c3fe9
Giancarlo Anemone <ganemone@gmail.com>
2015-02-13 00:28:29 -0500

    Add api helper functions

---
+++ client/Models/api.js
@@ -0,0 +1,35 @@
+var request = require('browser-request');
+
+function getResponseCB(cb) {
+  return function getResponseCBFunc(error, response) {
+    if (error) {
+      return cb(error);
+    }
+    return cb(null, response.response.objects);
+  }
+}
+
+exports.getAll = function getAll(model, cb) {
+  return request({
+    uri: '/api/' + model,
+    method: 'GET',
+    json: true
+  }, getResponseCB(cb));
+};
+
+exports.getByID = function getByID(model, id, cb) {
+  return request({
+    uri: '/api/' + model + '/' + id,
+    method: 'GET',
+    json: true
+  }, getResponseCB(cb));
+};
+
+exports.insertObject = function insertObject(modelName, data, cb) {
+  return request({
+    uri: '/api/' + modelName,
+    method: 'POST',
+    json: true,
+    body: data
+  }, getResponseCB(cb));
+};
